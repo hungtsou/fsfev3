6773989df7d9ebacbfbf4a520f8ca4570b36ad6b
hungtsou <hungtsougg@gmail.com>
2025-06-12 22:03:49 -0600

    refactor server implementation to use Express framework and update request handling

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var http = require('http');
-var fs = require('fs');
+const express = require('express');
+const fs = require('fs');
+const app = express();
 
-http.createServer(function(req, res){
+app.get('/', (req, res) => {
   console.log(req.url, 'hello request');
-  res.writeHead(200, {'Content-Type': 'text/html'});
   let html = fs.readFileSync(__dirname + '/index.html', 'utf8');
   html = html.replace('{title}', 'Hello World!');
-  res.end(html);
-}).listen(8080);
+  res.send(html);
+});
 
-console.log('Server is running on port 8080');
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
